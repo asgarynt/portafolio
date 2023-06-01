cf668c2d13508d86ccfc48a5210e4a1acbc89a68
Asgary Nieto Terrazas <asgarynt@gmail.com>
2023-06-01 13:50:36 -0600

    base update

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portafolio",
-  build: {
-    rollupOptions: {
-      external: ['/portafolio/assets/index-80765589.js'],
-    },
-  },
+  base: "https://asgarynt.github.io/portafolio",
+ 
 })
